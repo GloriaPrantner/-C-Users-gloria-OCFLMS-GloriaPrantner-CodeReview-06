80d85b429259c2cbcd10761a3de333bc69eb67e7
Gloria Prantner <gloriaprantner@gmail.com>
2020-06-20 00:12:13 +0200

    finished styling and regular tasks

--- script/script.js
+++ script/script.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
         }
         ;
         Location.prototype.display = function () {
-            return " \n\t\t<div id=\"" + this.name + "\" class=\"location\">\n\t\t\t<hr>\n\t\t\t<img class=\"img-fluid w-25\" src=\"" + this.img + "\" alt=\"" + this.name + "\"> <br>\n\t\t\t" + this.name + " <br>\n\t\t\t" + this.adress + " <br>\n\t\t\t" + this.zip + " " + this.city + " <br>\n\t\t\t<sup>Created: " + this.date + " " + this.time + "</sup>\t\n\t\t</div>";
+            return " \n\t\t\t<img class=\"rounded w-100 d-sm-none d-md-block\" src=\"" + this.img + "\" alt=\"" + this.name + "\"> <br>\n\t\t\t<span class=\"font-weight-bold\">" + this.name + "</span> <br>\n\t\t\t" + this.adress + " <br>\n\t\t\t" + this.zip + " " + this.city + " <br>\n\t\t\t<sup>Created: " + this.date + " " + this.time + "</sup><br>";
         };
         ;
         return Location;
@@ -51,7 +51,7 @@ $(document).ready(function () {
         }
         ;
         Restaurant.prototype.display = function () {
-            return "\n\t\t" + _super.prototype.display.call(this) + " \n\t\t<div>\n\t\t\t" + this.name + " is a " + this.type + " restaurant. <br>\n\t\t\tMore infos: <br>\n\t\t\tTel: " + this.num + " <br>\n\t\t\tWebsite: <a href=\"" + this.webSite + "\">" + this.webSite + "</a>  \n\t\t\t<hr>\n\t\t</div>";
+            return "\n\t\t\t" + _super.prototype.display.call(this) + " \n\t\t\t" + this.name + " is a " + this.type + " restaurant. <br>\n\t\t\tMore infos: <br>\n\t\t\tTel: " + this.num + " <br>\n\t\t\tWebsite: <a href=\"" + this.webSite + "\">" + this.webSite + "</a>";
         };
         ;
         return Restaurant;
@@ -70,7 +70,7 @@ $(document).ready(function () {
         }
         ;
         Event.prototype.display = function () {
-            return "\n\t\t" + _super.prototype.display.call(this) + " \n\t\t<div>\n\t\t\t" + this.name + " is on " + this.eventDate + " at " + this.eventTime + " <br>\n\t\t\tCost: <br>\n\t\t\t" + this.price + "\u20AC<br>\n\t\t\t<a href=\"" + this.webSite + "\">More infos</a>\n\t\t\t<hr>\n\t\t</div>";
+            return "\n\t\t\t" + _super.prototype.display.call(this) + " \n\t\t\t" + this.name + " is on " + this.eventDate + " at " + this.eventTime + " <br>\n\t\t\tCost: <br>\n\t\t\t" + this.price + "\u20AC<br>\n\t\t\t<a href=\"" + this.webSite + "\">More infos</a>";
         };
         ;
         return Event;
@@ -78,13 +78,15 @@ $(document).ready(function () {
     ;
     // data
     var loc0 = new Location("Schönbrunn Park", "Vienna", 1130, "Maxingstraße 13b", "img/loc0.jpg", "02.05.2020", "14:21");
-    var loc1 = new Location("St. Charles Church", "Vienna", 1010, "Karlsplatz 1", "img/loc1.jpg", "05.05.2020", "10:12");
+    var loc1 = new Location("St.Charles Church", "Vienna", 1010, "Karlsplatz 1", "img/loc1.jpg", "05.05.2020", "10:12");
     var rest0 = new Restaurant("BioFrische", "Vienna", 1150, "Stutterheimstraße 6", "img/rest0.jpg", "03.05.2020", "15:40", "+43(1) 9529215", "Indian", "biofrische.wien");
     var rest1 = new Restaurant("Ebi", "Vienna", 1210, "Leopoldauer Str 45-47", "img/rest1.jpg", "02.06.2020", "9:01", "+43 (1) 2703838", "Chinese", "https://www.ebi-vienna.at/ebi21");
     var eve0 = new Event("Die Toten Hosen", "Vienna", 1150, "Stadthalle", "img/eve0.jpg", "30.03.2020", "12:01", "4.7.2020", "20:00", "64,50", "https://www.dietotenhosen.de/");
-    var eve1 = new Event("Pizzera&Jaus", "Vienna", 1150, "Stadthalle", "img/eve1.jpg", "19.06.2020", "13:05", "17.04.2021", "19:30", "41", "http://www.paulpizzera.at/pizzera-jaus/");
+    var eve1 = new Event("Pizzera & Jaus", "Vienna", 1150, "Stadthalle", "img/eve1.jpg", "19.06.2020", "13:05", "17.04.2021", "19:30", "41", "http://www.paulpizzera.at/pizzera-jaus/");
+    // loop for printing in HTML
     for (var i = 0; i < arr.length; i++) {
-        $("#wrapping").append(arr[i].display());
+        $("#wrapping").append("<div id= \"box" + i + "\" class=\"location bg-info text-center rounded mt-3 p-3 col-sm-12 col-md-5 offset-md-1 col-lg-2 offset-lg-1\"> </div>");
+        $("#box" + i).append(arr[i].display());
     }
-    console.table(arr);
+    ;
 });
--- script/script.ts
+++ script/script.ts
@@ -12,7 +12,6 @@ class Location {
 	date="";
 	time="";
 
-
 constructor(name, city, zip, adress, img, date, time) {
 		this.name = name;
 		this.city = city;
@@ -26,14 +25,11 @@ constructor(name, city, zip, adress, img, date, time) {
 
 	display(){
 		return ` 
-		<div id="${this.name}" class="location">
-			<hr>
-			<img class="img-fluid w-25" src="${this.img}" alt="${this.name}"> <br>
-			${this.name} <br>
+			<img class="rounded w-100 d-sm-none d-md-block" src="${this.img}" alt="${this.name}"> <br>
+			<span class="font-weight-bold">${this.name}</span> <br>
 			${this.adress} <br>
 			${this.zip} ${this.city} <br>
-			<sup>Created: ${this.date} ${this.time}</sup>	
-		</div>`;
+			<sup>Created: ${this.date} ${this.time}</sup><br>`;
 	};
 };
 
@@ -51,17 +47,13 @@ constructor(name, city, zip, adress, img, date, time, num, type, webSite){
 	};
 	display(){
 		return `
-		${super.display()} 
-		<div>
+			${super.display()} 
 			${this.name} is a ${this.type} restaurant. <br>
 			More infos: <br>
 			Tel: ${this.num} <br>
-			Website: <a href="${this.webSite}">${this.webSite}</a>  
-			<hr>
-		</div>`;
+			Website: <a href="${this.webSite}">${this.webSite}</a>`;
 	};
 };
-
 
 // child class 2
 class Event extends Location {
@@ -79,34 +71,30 @@ constructor(name, city, zip, adress, img, date, time, eventDate, eventTime, pric
 	};
 	display(){
 		return `
-		${super.display()} 
-		<div>
+			${super.display()} 
 			${this.name} is on ${this.eventDate} at ${this.eventTime} <br>
 			Cost: <br>
 			${this.price}€<br>
-			<a href="${this.webSite}">More infos</a>
-			<hr>
-		</div>`;
-
+			<a href="${this.webSite}">More infos</a>`;
 	};
-
 };
 
 
 // data
 let loc0 = new Location ("Schönbrunn Park", "Vienna", 1130, "Maxingstraße 13b", "img/loc0.jpg", "02.05.2020", "14:21");
-let loc1 = new Location ("St. Charles Church", "Vienna", 1010, "Karlsplatz 1", "img/loc1.jpg", "05.05.2020", "10:12");
+let loc1 = new Location ("St.Charles Church", "Vienna", 1010, "Karlsplatz 1", "img/loc1.jpg", "05.05.2020", "10:12");
 
 let rest0 = new Restaurant("BioFrische", "Vienna", 1150, "Stutterheimstraße 6", "img/rest0.jpg", "03.05.2020", "15:40",  "+43(1) 9529215", "Indian", "biofrische.wien");
 let rest1 = new Restaurant("Ebi", "Vienna", 1210, "Leopoldauer Str 45-47", "img/rest1.jpg", "02.06.2020", "9:01", "+43 (1) 2703838", "Chinese", "https://www.ebi-vienna.at/ebi21");
 
 let eve0 = new Event("Die Toten Hosen", "Vienna", 1150, "Stadthalle", "img/eve0.jpg","30.03.2020", "12:01", "4.7.2020", "20:00", "64,50", "https://www.dietotenhosen.de/");
-let eve1 = new Event("Pizzera&Jaus", "Vienna", 1150, "Stadthalle", "img/eve1.jpg","19.06.2020", "13:05", "17.04.2021", "19:30", "41", "http://www.paulpizzera.at/pizzera-jaus/");
 
+let eve1 = new Event("Pizzera & Jaus", "Vienna", 1150, "Stadthalle", "img/eve1.jpg","19.06.2020", "13:05", "17.04.2021", "19:30", "41", "http://www.paulpizzera.at/pizzera-jaus/");
+
+
+// loop for printing in HTML
 for(let i = 0; i < arr.length; i++){
-	$("#wrapping").append(arr[i].display());
-}
-
-console.table(arr);
-
+	$("#wrapping").append(`<div id= "box${i}" class="location bg-info text-center rounded mt-3 p-3 col-sm-12 col-md-5 offset-md-1 col-lg-2 offset-lg-1"> </div>`)
+	$("#box" + i).append(arr[i].display());
+	};
 })
